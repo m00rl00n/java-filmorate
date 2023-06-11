6d121fd61181fc3ef91c4d27928f2d676e2f1bb8
moorloon <moorloon@yandex.ru>
2023-06-11 12:45:15 +0700

    'учтены правки ревьюера'

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/SortByOption.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.model;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public enum SortByOption {
+    LIKES("likes"),
+    YEAR("year");
+
+    private final String value;
+
+    SortByOption(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static SortByOption fromValue(String value) {
+        if (value != null) {
+            for (SortByOption option : values()) {
+                if (option.value.equalsIgnoreCase(value)) {
+                    return option;
+                }
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+    }
+}
